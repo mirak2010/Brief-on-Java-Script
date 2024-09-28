b71618dcc42c3042f29142ad4f66a5a87c7bbe0a
mirak2010 <mirakhmad0101@yahoo.com>
2024-09-28 10:25:27 +0500

    Done

---
+++ datatypescw.js
@@ -0,0 +1,24 @@
+var a=7;
+var b=8;
+var c=12;
+var d=12;
+var e=null;
+var f
+
+document.write("variable a is equals to:" + (a==b));
+document.write("<br>");
+document.write("variable b is equals to:" + b);
+document.write("<br>");
+document.write("variables c and d are equal:" + (c==d));
+document.write("<br>");
+document.write("variable e:"+ e);
+document.write("<br>");
+document.write("variable f is equal to:" + f);
+document.write("<br>");
+{
+    let a = 7;
+    const b = 10;
+    document.write(a + b)
+}
+document.write("<br>");
+document.write(a+b)
